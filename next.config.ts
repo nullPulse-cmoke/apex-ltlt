cad7790c0236d1f1f54a1243a3a6e7c553471140
APEX Deployer <>
2026-08-29 15:47:08 +0500

    perf: skip typechecking during build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  typescript: {
+    ignoreBuildErrors: true, // Speeds up build significantly on AWS
+  },
 };
 
 export default nextConfig;
